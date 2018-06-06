070c571e848b211eb1083efaaa25a55c970ae724
xmqian <2228918710@qq.com>
2018-06-06 17:34:45 +0800

    添加父activity

--- utilslibrary/src/main/java/regpang/utilslibrary/base/BaseActivity.java
+++ utilslibrary/src/main/java/regpang/utilslibrary/base/BaseActivity.java
@@ -1,8 +1,15 @@
 package regpang.utilslibrary.base;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhangfeng on 2016/4/28.
@@ -11,6 +18,12 @@ import android.view.View;
 public abstract class BaseActivity extends AppCompatActivity {
     protected boolean isFrist = true;//判断数据是否第一次加载
     private String title;
+    private static List<BaseActivity> caches = new ArrayList<>();
+    public static Class<?> currentActivity;
+    /**
+     * 用来标记同一生命周期
+     */
+    private String tag = "";
 
 
     @Override
@@ -21,6 +34,13 @@ public abstract class BaseActivity extends AppCompatActivity {
         //检查网络
         initView();
         initListener();
+        caches.add(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currentActivity = getClass();
     }
 
     /**
@@ -44,6 +64,24 @@ public abstract class BaseActivity extends AppCompatActivity {
         this.title = title;
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean onKey = true;
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                return keyBack();
+            default:
+                onKey = super.onKeyDown(keyCode, event);
+                break;
+        }
+        return onKey;
+    }
+
+    public boolean keyBack() {
+        finish();
+        return true;
+    }
+
     /**
      * 自动转控件类型
      *
@@ -65,5 +103,32 @@ public abstract class BaseActivity extends AppCompatActivity {
      */
     protected <V> V findView(View paramView, int paramInt) {
         return (V) paramView.findViewById(paramInt);
+    }
+
+    /**
+     * 隐藏状态栏开关
+     *
+     * @param on
+     */
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    public static void exit() {
+        for (BaseActivity cache : caches) {
+            cache.finish();
+        }
+        caches.clear();
+        System.exit(0);
+        System.out.println("#exit app");
     }
 }
