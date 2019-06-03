6af89d4cef2f9cdbfaa528f0d502b0302a338829
companyxmqian <2228918710@qq.com>
2019-06-03 20:59:46 +0800

    测试lambda表达式

--- app/src/test/java/com/example/ExampleUnitTestLibrary.java
+++ app/src/test/java/com/example/ExampleUnitTestLibrary.java
@@ -2,6 +2,11 @@ package com.example;
 
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import kotlin.text.Regex;
+
 /**
  * @author xmqian
  * @date 19-5-4
@@ -12,6 +17,15 @@ public class ExampleUnitTestLibrary {
 
     @Test
     public void test() {
-        System.out.println("单元测试");
+        output output = (s) -> s + "参与";
+        String bankNo = "6217001630047494627";
+        String matcher = Matcher.quoteReplacement("/^(\\d{4})\\d+(\\d{4})$/");
+        String rp = bankNo.replace(matcher, "$1 **** **** $2");
+        System.out.print(output.i("这是测试")+"\n"+rp);
+
+    }
+
+    private interface output {
+        public String i(String s);
     }
 }
